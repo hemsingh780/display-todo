e0f1105c52724d3508197b51a70d5ca12bd3f980
hem <hemsinghdhoni_bca18_21@its.edu.in>
2021-06-14 12:43:30 +0530

    add display todo in github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
+const [state,setState] = useState({
+  inputValue:''
+})
+
+const [todo,setTodo]  = useState({
+  todoArr:[]
+})
+
+
+const valueStoreAndChange = (e) =>{
+  if(e.key === 'Enter'){ 
+   let arr = [...todo.todoArr]
+   arr.push(state.inputValue)
+   setTodo({todoArr:arr})
+   setState({inputValue:''})
+  }
+}
+
+
+
+const change = (e) =>{
+   setState({inputValue:e.target.value}) 
+}
+
+let show = todo.todoArr.map((x,i) => {
+  return (<p className='P' key={i}>{x}</p>)
+})
+
+return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h2>Todo</h2>
+            <input type='text'
+             value={state.inputValue}
+             onChange={change}
+             onKeyDown={valueStoreAndChange}/>
+           {show}
     </div>
+
   );
 }
 
